f9d67cc142c688f7e899bc70aad686419394d52f
gituser <danjee2009@gmail.com>
2025-09-09 20:14:14 +0900

    폴더 구조

---
+++ Orato/Orato/Source/Application/FirstView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstView.swift
+//  Orato
+//
+//  Created by maple on 9/9/25.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstView()
+}
