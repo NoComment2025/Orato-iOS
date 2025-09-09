a132ecdbc3ac0dde1864b07abcc44e4e3efd8006
gituser <danjee2009@gmail.com>
2025-09-09 20:07:31 +0900

    first

---
+++ Orato/Orato/OratoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OratoApp.swift
+//  Orato
+//
+//  Created by maple on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct OratoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
