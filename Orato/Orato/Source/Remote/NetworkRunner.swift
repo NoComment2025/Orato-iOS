d56ef4fa86fb2090e2a5c78978d0fdacba292600
gituser <danjee2009@gmail.com>
2025-09-09 20:41:55 +0900

    net

---
+++ Orato/Orato/Source/Remote/NetworkRunner.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkRunner.swift
+//  Orato
+//
+//  Created by maple on 9/9/25.
+//
+
+import SwiftUI
+
+struct NetworkRunner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkRunner()
+}
