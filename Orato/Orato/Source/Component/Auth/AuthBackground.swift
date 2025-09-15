03d6d05de3778852e88596c26d82e7e19a9ae8cc
gituser <danjee2009@gmail.com>
2025-09-15 20:47:58 +0900

    TabView & Feature

---
+++ Orato/Orato/Source/Component/Auth/AuthBackground.swift
@@ -0,0 +1,18 @@
+//
+//  AuthBackground.swift
+//  Orato
+//
+//  Created by maple on 9/15/25.
+//
+
+import SwiftUI
+
+struct AuthBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthBackground()
+}
---
+++ Orato/Orato/Source/Feature/Mypage/ProfileViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewModel.swift
+//  Orato
+//
+//  Created by maple on 9/15/25.
+//
+
+import SwiftUI
+
+struct ProfileViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileViewModel()
+}
